86fffc422c96d1ae1d5cc573b407bed77e46dd2d
shuta13 <hollystarsun@gmail.com>
2019-10-01 13:24:11 +0900

    :boom: ディレクトリの構造組み直した

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Hoge from './pages/Hoge'
+import Hoge from '../components/Hoge'
 
 const App: React.FC = () => (
   <Router>
--- src/test/App.spec.tsx
+++ src/test/App.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../pages/App';
 
 describe('なんか最初からあるやつ書き方キモいので修正', () => {
   test('renders without crashing', () => {
